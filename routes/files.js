3844e42838b725cb987c33f5ef9128c02dffbb94
amanu9 <amanuelbeyene47@gmail.com>
2025-06-14 11:16:13 +0300

    multifileuplod

--- routes/files.js
+++ routes/files.js
@@ -4,7 +4,10 @@ const {uploadFile}=require("../controllers/file")
 const { authenticate } = require("../middleware");
 const upload=require("../middleware/upload")
 
-router.post("/upload",authenticate,upload.single("image"),uploadFile);//here single is middle ware and image is the feild name 
+//upload.array("images", 10) //   to limit the Maximum 10 files if we need like this
+router.post("/upload",authenticate,upload.single("image"),uploadFile);//here single is middle to upload single file ware and image is the feild name to make it multiplr
+// change the single to array 
+//router.post("/upload",authenticate,upload.array("image"),uploadFile);//for multiple file use array instead of single
 
 
 
